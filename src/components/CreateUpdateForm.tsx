a7ca714661474f9a5b06677543d499701a2bfebe
Dalibor Pavicic <dalibor.pavicic@tymeshift.com>
2022-01-27 11:42:31 +0100

    replace plain HTML with antd components

--- src/components/CreateUpdateForm.tsx
+++ src/components/CreateUpdateForm.tsx
@@ -1,23 +1,24 @@
+import { Button, Form, Input, Select } from 'antd'
 import React from 'react'
 
 export function CreateUpdateForm() {
   return (
-    <form action="/" method="post">
-      <div>
-        <label htmlFor="reasonName">Name</label>
-        <input id="reasonName" type="text" placeholder="For ex.: Call Out" />
-      </div>
-      <div>
-        <label htmlFor="reasonType">Type</label>
-        <select id="reasonType">
-          <option>Planned</option>
-          <option>Unplanned</option>
-        </select>
-      </div>
-      <div>
-        <button type="reset">Cancel</button>
-        <button type="submit">Add Reason</button>
-      </div>
-    </form>
+    <Form action="/" method="post">
+      <Form.Item label="Name" htmlFor="reasonName">
+        <Input id="reasonName" type="text" placeholder="For ex.: Call Out" />
+      </Form.Item>
+      <Form.Item label="Type" htmlFor="reasonType">
+        <Select id="reasonType" value="planned">
+          <Select.Option value="planned">Planned</Select.Option>
+          <Select.Option value="unplanned">Unplanned</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="reset">Cancel</Button>
+        <Button htmlType="submit" type="primary">
+          Add Reason
+        </Button>
+      </Form.Item>
+    </Form>
   )
 }
