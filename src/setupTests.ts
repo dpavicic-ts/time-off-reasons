c41964ff93693f490bf76da7268aefdeb4946a66
Dalibor Pavicic <dalibor.pavicic@tymeshift.com>
2022-01-27 11:41:23 +0100

    add and set up `antd`

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+// Mock the window.matchMedia that is used in Ant design, but not in jsdom
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }
+  }
